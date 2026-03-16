9f8736c07f3735579eb5a16b0c2004ca8e709909
chalish <git@chalish.dev>
2026-03-16 18:42:17 +0300

    Sphere line intersection demo

--- packages/playground/src/demos/demos.tsx
+++ packages/playground/src/demos/demos.tsx
@@ -8,6 +8,7 @@ import Demo6 from "./demo6";
 import Demo7 from "./demo7";
 import Demo8 from "./demo8";
 import Demo9 from "./demo9";
+import Demo10 from "./demo10";
 
 const demos = {
   demo1: {
@@ -45,6 +46,10 @@ const demos = {
   demo9: {
     title: "Gravity Sculptor",
     component: <Demo9 />,
+  },
+  demo10: {
+    title: "Sphere-Line Explorer",
+    component: <Demo10 />,
   },
 };
 
